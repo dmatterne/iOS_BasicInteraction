b4eb30f5c45a28c48b56a31e4d88d790513a1973
David Matterne <david.matterne@gmail.com>
2016-09-29 09:36:47 +0200

    Exercise completed

--- iOS_BasicInteraction/ViewController.swift
+++ iOS_BasicInteraction/ViewController.swift
@@ -6,10 +6,13 @@
 //  Copyright © 2016 Training. All rights reserved.
 //
 
-import UIKit 
+import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func clickRedButton(_ sender: AnyObject) {
+        
+        myLabel.text = "ABORT ABORT ABORT"
+        myLabel.textColor = UIColor.red
+        
+    }
+    
+    @IBAction func clickGreenButton(_ sender: AnyObject) {
+        
+        myLabel.text = "GREEN GREEN SUPERGREEN"
+        myLabel.textColor = UIColor.green
+        
+    }
 
 }
 
